109269af00ef921f076b8723be8c625741a82e26
cscim5 <cscim918@gmail.com>
2023-02-26 17:52:46 +0900

    RestController 넣지 말기!

--- src/main/java/com/mydias/mydias/controller/UserController.java
+++ src/main/java/com/mydias/mydias/controller/UserController.java
@@ -9,15 +9,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Controller
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/user")
 public class UserController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
